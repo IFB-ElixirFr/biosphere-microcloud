44f788f02b36074f446078b2e1bc6a40be91162a
lburlot <39338010+lburlot@users.noreply.github.com>
2019-01-14 18:49:37 +0100

    Update install_microscope.sh

--- frontend/install_microscope.sh
+++ frontend/install_microscope.sh
@@ -62,7 +62,9 @@ $mysql_request GO_RES < $oid_data_dir/GO_RES_ASid1_ASid2_data.sql
 
 # Copy web data
 cd "web_data"
-cp -r Acinetobacter_sp_ADP1/ /var/www/agc_data/
+cp -r Acinetobacter_sp_ADP1/* /var/www/agc_data/Acinetobacter_sp_ADP1/
+chown -R root:apache /var/www/agc_data/*
+chmod -R u=rwX,g=rX,o=rX /var/www/agc_data/*
 
 # Set values in configuration file
 cd "../../$1"
