a1e02ef1fae0ddf4adbd975430cdb066fced9fdd
lburlot <lburlot@genoscope.cns.fr>
2020-04-22 23:09:08 +0200

    Add on VM add/remove code.

---
+++ slave/07_on_VM_remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /scripts/allows_other_to_access_me.sh --dry-run
+
+auto_gen_users
+publish_pubkey
+allow_others
+
+source /scripts/populate_hosts_with_components_name_and_ips.sh --dry-run
+
+populate_hosts_with_components_name_and_ips_on_vm_remove hostname
+
