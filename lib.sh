0b7b8a625dc940cc198f3ebfe7d5d8c92b7dc209
Mathieu Dubois <mdubois@genoscope.cns.fr>
2020-04-27 18:14:59 +0200

    Format.

--- lib.sh
+++ lib.sh
@@ -11,7 +11,8 @@
 
 # This function is copied from biosphere-commnons (NFS_ready)
 # with adaptations for MicroCloud.
-NFS_microcloud_ready(){
+NFS_microcloud_ready()
+{
     ss-get --timeout=3600 nfsserver_is_ready
     nfs_ready=$(ss-get --timeout=3600 nfsserver_is_ready)
     msg_info "Waiting NFS to be ready."
@@ -28,20 +29,20 @@ NFS_microcloud_mount()
 {
     if [[ $# -lt 2 ]]; then
         echo "This function expects 2 directories in argument !"
-    else    
+    else
         SHARED_DIR=$1
         MOUNT_DIR=$2
         msg_info "Mount $MOUNT_DIR where shared directory is $SHARED_DIR from $NFSSERVER_HOSTNAME host)."
         if [ ! -d "$MOUNT_DIR" ]; then
             msg_info "$MOUNT_DIR doesn't exist !"
-        else    
+        else
             msg_info "Mounting $MOUNT_DIR..."
-    
+
             umount $MOUNT_DIR
             mount $NFSSERVER_HOSTNAME:$SHARED_DIR $MOUNT_DIR 2>/tmp/mount_error_message.txt
             ret=$?
             msg_info "$(cat /tmp/mount_error_message.txt)"
-        
+
             if [ $ret -ne 0 ]; then
                 ss-abort "$(cat /tmp/mount_error_message.txt)"
             else
